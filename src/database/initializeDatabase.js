49686bab8e3a7d2a0308413f8ff5725aebf9ac2e
HenriquePellim <159555857+HenriquePellim@users.noreply.github.com>
2024-11-23 02:02:24 +0000

    [feat] Aula 15 18:01

--- src/database/initializeDatabase.js
+++ src/database/initializeDatabase.js
@@ -1,11 +1,13 @@
 export async function initializeDatabase(database) {
- try {
-  await database.execAsync(`
-    DROP TABLE IF EXISTS payment;
+  try {
+    await database.execAsync(`
+    /* DROP TABLE IF EXISTS payment;
     
     DROP TABLE IF EXISTS users;
 
-    
+    DROP INDEX IF EXISTS idx_users_nome;
+
+    DROP INDEX IF EXISTS idx_payments_data_pagamento; */
 
     CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -23,6 +25,7 @@ export async function initializeDatabase(database) {
      user_cadastro INTEGER NOT NULL,
      valor_pago REAL NOT NULL,
      data_pagamento DATE NOT NULL,
+     numero_recibo TEXT NOT NULL,
      observacao TEXT,
       created_at DATE DEFAULT CURRENT_TIMESTAMP,
       updated_at DATE,
@@ -30,11 +33,16 @@ export async function initializeDatabase(database) {
       FOREIGN KEY (user_cadastro) REFERENCES users(id)
     );
 
-    INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super', 'super@email.com', 'A123456a!', 'SUPER');
+    CREATE INDEX IF NOT EXISTS indx_users_nome ON users (nome);
+
+     CREATE INDEX IF NOT EXISTS indx_payments_data_pagamento ON payments (data_pagamento);
+
+    /* INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Super', 'super@email.com', 'A123456a!', 'SUPER');
     INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('Admin', 'admin@email.com', 'A123456a!', 'ADMIN');
-    INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('User', 'user@email.com', 'A123456a!', 'USER');
+    INSERT OR REPLACE INTO users (nome, email, senha, role) VALUES ('User', 'user@email.com', 'A123456a!', 'USER'); */
+    
   `);
- } catch (error) {
+  } catch (error) {
     console.log(error);
- }
+  }
 } 
